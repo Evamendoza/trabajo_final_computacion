f6247c077dee76fc6e9d490bffeccf556284f17d
Evamendoza <82737364+Evamendoza@users.noreply.github.com>
2024-05-17 21:27:24 +0200

    Modicación
    Mejora de las formulas e intento  movimiento al pegarse

---
+++ pairing.cc
@@ -0,0 +1,100 @@
+#include <base.hh>
+#include <environment.hh>
+#include <cmath>
+
+#include "pairing.hh"
+
+// Environment class to obtain the size of the environment
+extern mrs::Environment env;
+
+// Constructor
+PairingRobot::PairingRobot(unsigned int id, const mrs::Position2d &p,
+                           const mrs::RobotSettings &settings,
+                           const mrs::Velocity2d &vel) : mrs::Robot(id, p, settings)
+{
+}
+
+// Action (main) method
+const mrs::Velocity2d &PairingRobot::action(std::vector<mrs::RobotPtr> &neigh) {
+    m_vel =  mrs::Velocity2d::Random(); // Velocidad inicial aleatoria por defecto
+    
+    //m_vel=mrs::Velocity2d();
+    const float closeThreshold = 1; // Distancia para considerar "muy cerca"
+    const float minDistance = 0.5; // Distancia mínima que deben mantener
+    bool foundPartner = false;
+    int contador = 0;
+    float vx;
+    float vy;
+    float ux,uy;
+    
+    bool emparejado=false;
+    
+    for (auto &n : neigh) {
+        // Calcula la distancia entre el robot actual y el robot vecino
+        float dist = mrs::distance(m_pos, n->position())-minDistance;
+        float angulo =mrs::angle(m_pos, n->position());
+
+          if ((settings().color[0] != n->settings().color[0])&& emparejado==false) {
+            
+            vx=dist*cos(angulo)/3.0;
+            vy=dist*sin(angulo)/3.0;
+            
+            m_vel={vx +m_vel[0],vy+m_vel[1]};
+            m_vel =0.5*m_vel;
+
+            
+
+            /*if (dist < closeThreshold) {
+                foundPartner = true;
+       velocity         mrs::Position2d direction = n->position() - m_pos;
+                direction.normalize(); // Normalizar la dirección
+
+                if (dist > minDistance) {
+                    // Si la distancia es mayor que la mínima, moverse hacia el otro robot
+                    //m_vel = direction * settings().vMin; // Ajustar a la velocidad mínima permitida
+                    m_vel
+                } else if (dist <= minDistance) {
+                    // Si están demasiado cerca, ajustar la velocidad para mantener la distancia mínima
+                    m_vel = direction * (settings().vMin * (dist / minDistance));
+                    foundPartner = true;
+                }
+
+                
+            }*/
+        }
+       if ((dist<=0.6)&&(settings().color[0] != n->settings().color[0])){
+            //ux=n->velocity()/2;
+            m_vel=n->velocity();
+            emparejado=true;
+         }
+       else if (emparejado==true){
+         vx=dist*cos(angulo);
+         vy=dist*sin(angulo);
+            
+         m_vel={-vx +m_vel[0],-vy+m_vel[1]};
+
+         m_vel=2.1*m_vel;
+        
+        
+       }
+
+
+        //
+
+        // Verifica si el color es opuesto basado en la primera componente
+        //settings().color[0] != n->settings().color[0]
+      
+    }
+
+    // Si no se encontró ningún robot para emparejar, mantiene la velocidad aleatoria inicial
+    /*if (!foundPartner) {
+        m_vel = mrs::Velocity2d::Random();
+    }*/
+
+    return m_vel;
+}
+
+mrs::RobotPtr PairingRobot::clone() const {
+    PairingRobotPtr newRobot = std::make_shared<PairingRobot>(m_id, m_pos, m_settings, m_vel);
+    return std::dynamic_pointer_cast<mrs::Robot>(newRobot);
+}
